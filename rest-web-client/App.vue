e5c9a79c9505ead9e80fc78b1e1e038a2d3e8106
andres.munozg@utem.clgit config --global user.name Tu <andres.munozg@utem.cl>
2020-05-08 21:01:04 -0400

    se incorporó CSS

--- rest-web-client/App.vue
+++ rest-web-client/App.vue
@@ -1,22 +1,24 @@
 <template> 
 <div>
-  
-  
+
   <h1>Cliente para REST web services </h1>
-  <hr/>
-   <input type="text" placeholder="Ingrese Rut..." v-model="rut"> &nbsp; 
-   <button @click="enviaRut" > Comprobar rut</button>
-   <h3>{{estadorut}}</h3>
+
+  <h3> Validador de rut </h3>
+   <input type="text" placeholder="Ingrese Rut..." v-model="rut">&nbsp;
+   <button  @click="enviaRut" > Comprobar RUT</button>
+   <h4>{{estadorut}}</h4>
     
-    <hr/>
-    <ul>
-    <li><input type="text" placeholder="Ingrese Nombre..." v-model="nombres"> &nbsp;</li>
-    <li><input type="text" placeholder="Ingrese Paterno..." v-model="paterno"> &nbsp;</li>
-    <li><input type="text" placeholder="Ingrese Materno.." v-model="materno"> &nbsp;</li>
-    <li><input type="text" placeholder="Ingrese Genero..." v-model="genero"> &nbsp;</li>
+    <h3>Generador de Saludo</h3>
+   
+    
+    <input type="text" placeholder="Ingrese Nombres..." v-model="nombres"> <br/>
+    <input type="text" placeholder="Ingrese Paterno..." v-model="paterno"> <br/>
+    <input type="text" placeholder="Ingrese Materno..." v-model="materno"> <br/>
+    <input type="text" placeholder="Ingrese Genero (F,M)..." v-model="genero">
+    <br/> <br/>
     <button @click="enviaSaludo" > Generar Saludo</button>
-    <h3>{{estadosaludo}}</h3>
-   </ul>
+    <h4>{{estadosaludo}}</h4>
+  
 
 
 </div>
@@ -26,18 +28,18 @@
 </template>
 
 <script>
-import axios from "axios"; //importamos el cliente http
+import axios from "axios";
 
 export default {
   data() {
     return {
-      rut: null, // Para validarut
-      estadorut: null, // me devuelve el estado del rut
-      nombres: null, // Para saludo
-      paterno: null, // Para saludo
-      materno: null, // Para saludo
-      genero: null, // Para saludo
-      estadosaludo: null // me devuelve el saludo
+      rut: null,
+      estadorut: null,
+      nombres: null,
+      paterno: null,
+      materno: null,
+      genero: null,
+      estadosaludo: null
     };
   },
   mounted() {},
@@ -61,17 +63,13 @@ export default {
       //funcion nueva que enviara los valores ingresados a las propiedades de saludo
       axios
         .post("http://localhost:3000/saludo", {
-          nombres: this.nombres ,
-          paterno:this.paterno,
+          nombres: this.nombres,
+          paterno: this.paterno,
           materno: this.materno,
           genero: this.genero
         })
         .then(response => {
           console.log(response);
-          //this.nombres = response.data;
-          //this.paterno = response.data;
-          //this.materno = response.data;
-          //this.genero = response.data;
           this.estadosaludo = response.data;
         });
     }
@@ -80,4 +78,38 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: gainsboro;
+}
+button {
+  background-color: rgba(190, 53, 53, 0.952);
+  padding: 4px;
+  font-weight: 500;
+  color: white;
+  border-radius: 100px;
+  border: 0px;
+}
+button:hover {
+  color: rgb(96, 122, 209);
+  background-color: #ffffff;
+}
+h1 {
+  font-family: Arial;
+  color: white;
+  font-size: 25px;
+  text-align: center;
+  background: rgba(190, 53, 53, 0.952);
+  padding: 20px;
+  border-radius: 25px;
+}
+h3 {
+  font-family: Arial;
+  color: rgb(53, 21, 21);
+  font-size: 25px;
+}
+h4 {
+  font-family: Arial;
+  color: rgb(96, 122, 209);
+  font-size: 19px;
+}
 </style>
